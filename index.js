e6b5d65bba3a4ae9e367fac101b53f5524ea3e70
tecnopulsar <tecnopulsar@gmail.com>
2023-04-11 13:05:35 -0300

    add cors sin /dolar

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 import express from "express";
 import axios from 'axios'
+import cors from 'cors'
 import * as cheerio from 'cheerio';
 
 const app = express();
 
+// var corsOptions = {
+//     origin: 'http://localhost:5173',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+
 const PORT = process.env.PORT || 5000;
 
-app.get("/dolar", async (req, res) => {
+app.get("/", cors(), async (req, res) => {
     try {
         const {data} = await axios.get('https://www.bcentral.cl/inicio');
 
         const $ = cheerio.load(data)
         const selectorDolar = 
-        "#_BcentralIndicadoresViewer_INSTANCE_pLcePZ0Eybi8_myTooltipDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(3) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2"
+        "#market-scrll-2 > tbody > tr:nth-child(2) > td.buy > a > div > div.buy-valueDelegate > div > div > div.fin-indicators-col1 > div > div > div:nth-child(3) > div > p.basic-text.fs-2.f-opensans-bold.text-center.c-blue-nb-2"
   
         const objetoValores = {
             fecha : new Date().toLocaleDateString(),
